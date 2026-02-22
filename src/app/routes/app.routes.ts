f154910f5ba484b3af9aec74b38856ab721b875a
GeronimoGM <geronimogonzalezmartino@gmail.com>
2026-02-22 16:32:18 -0300

    feat: add server-side rendering support and refactor app configuration
    - Updated package.json to include SSR dependencies and a new serve command.
    - Removed old app.config.ts and created app.config.server.ts for server-side configuration.
    - Introduced new routing files for server and client.
    - Enhanced ThemeService to check platform before accessing localStorage.
    - Added main.server.ts for bootstrapping the server application.
    - Created server.ts to handle requests and serve the Angular application.
    - Updated tsconfig.app.json to include Node types for server-side code.

--- src/app/routes/app.routes.ts
+++ src/app/routes/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./pages/index-page/index-page').then((m) => m.IndexPage),
+    loadComponent: () => import('../pages/index-page/index-page').then((m) => m.IndexPage),
   },
   {
     path: '**',
--- src/app/services/theme-service.ts
+++ src/app/services/theme-service.ts
@@ -1,4 +1,5 @@
-import { Injectable, signal } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { inject, Injectable, PLATFORM_ID, signal } from '@angular/core';
 import { Theme } from '../types/theme';
 
 @Injectable({
@@ -6,30 +7,35 @@ import { Theme } from '../types/theme';
 })
 export class ThemeService {
   constructor() {
-    const theme = localStorage.getItem(this.THEME_KEY);
+    if (isPlatformBrowser(this.platformId)) {
+      const theme = localStorage.getItem(this.THEME_KEY);
 
-    if (theme) {
-      this.setTheme(theme as Theme);
-    } else {
-      this.setTheme('system');
+      if (theme) {
+        this.setTheme(theme as Theme);
+      } else {
+        this.setTheme('system');
+      }
     }
   }
 
+  private readonly platformId = inject(PLATFORM_ID);
   private readonly THEME_KEY = 'theme';
   private readonly theme = signal('system' as Theme);
   readonly selectedTheme = this.theme.asReadonly();
 
   setTheme(theme: Theme) {
-    this.theme.set(theme);
+    if (isPlatformBrowser(this.platformId)) {
+      this.theme.set(theme);
 
-    const html = document.documentElement;
-    html.classList.remove('light', 'dark');
+      const html = document.documentElement;
+      html.classList.remove('light', 'dark');
 
-    if (theme === 'system') {
-      localStorage.removeItem(this.THEME_KEY);
-    } else {
-      localStorage.setItem(this.THEME_KEY, theme);
-      html.classList.add(theme);
+      if (theme === 'system') {
+        localStorage.removeItem(this.THEME_KEY);
+      } else {
+        localStorage.setItem(this.THEME_KEY, theme);
+        html.classList.add(theme);
+      }
     }
   }
 }
